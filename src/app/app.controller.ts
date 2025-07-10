08f6e8bee91e1afd1d30b98a66688f7088b58cb0
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-10 15:03:32 +0700

    docs(api): add swagger doc to app controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiServiceUnavailableResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
 @Controller('/')
 export class AppController {
   @Get('/health')
+  @ApiOkResponse({
+    description: 'Health check endpoint',
+    content: {
+      'text/plain': {
+        schema: {
+          type: 'string',
+          example: 'Ok!',
+        },
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
+  @ApiServiceUnavailableResponse({
+    description: 'Service unavailable',
+  })
   getHealth(): string {
     return 'Ok!';
   }
