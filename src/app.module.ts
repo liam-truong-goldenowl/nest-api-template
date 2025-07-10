67191647c07f66f0a78634ea5d7f2336c36fce16
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-10 11:15:47 +0700

    refactor: move env schema to constants folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
-import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { configuration } from '@/config';
+import { EnvSchema } from './utils/constants';
 import { TasksModule } from '@/api/tasks/tasks.module';
 import { UsersModule } from '@/api/users/users.module';
 import { DatabaseModule } from '@/database/database.module';
-
-const EnvSchema = Joi.object({
-  NODE_ENV: Joi.string()
-    .valid('development', 'production', 'test')
-    .default('development'),
-  PORT: Joi.number().port().default(3000),
-  DB_HOST: Joi.string().default('localhost'),
-  DB_PORT: Joi.number().port().default(5432),
-  DB_USERNAME: Joi.string().default('postgres'),
-  DB_PASSWORD: Joi.string().default('password'),
-  DB_NAME: Joi.string().default('nest'),
-  DB_SSL: Joi.boolean().default(false),
-});
 
 @Module({
   imports: [
--- src/utils/constants/envs.ts
+++ src/utils/constants/envs.ts
@@ -1,5 +1,20 @@
+import * as Joi from 'joi';
+
 export const Env = {
   PRODUCTION: 'production',
   DEVELOPMENT: 'development',
   TEST: 'test',
 };
+
+export const EnvSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().port().default(3000),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().port().default(5432),
+  DB_USERNAME: Joi.string().default('postgres'),
+  DB_PASSWORD: Joi.string().default('password'),
+  DB_NAME: Joi.string().default('nest'),
+  DB_SSL: Joi.boolean().default(false),
+});
