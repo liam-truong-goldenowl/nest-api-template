3a9789c76a7625cfe5bb04bfd70c423343bb5b21
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-11 10:06:43 +0700

    style: sort import statements

--- src/api/tasks/tasks.service.ts
+++ src/api/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 import {
   Injectable,
-  BadRequestException,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
